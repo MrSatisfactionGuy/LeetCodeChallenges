31d936f05ac85ea82a3cf662e4f9d06b7c867586
MrSatisfactionGuy <edd.wag18@gmail.com>
2024-06-18 20:09:41 +0200

    LeetCodeChallenges-66

---
+++ LeetCodeChallenges/0066_LeetCode_PlusOne/0066_LeetCode_PlusOne/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _0066_LeetCode_PlusOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits1 = { 1, 2, 3 };
+            Console.WriteLine(string.Join(", ", PlusOne(digits1)));  // Output: 1, 2, 4
+
+            int[] digits2 = { 9, 9, 9 };
+            Console.WriteLine(string.Join(", ", PlusOne(digits2)));  // Output: 1, 0, 0, 0
+        }
+
+        public static int[] PlusOne(int[] digits)
+        {
+            int n= digits.Length;
+
+            for(int i = n -1; i>= 0; i--)
+            {
+                if (digits[i] > 9)
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+            int[] newDigits = new int[n + 1];
+            newDigits[0] = 1;
+
+            return newDigits;
+        }
+
+    }
+}
